5f1486ebfc64005577bd8dc0628438f43562a137
arthurchen1989 <35855341+arthurchen1989@users.noreply.github.com>
2018-02-07 00:23:30 -0500

    updated the HW1

---
+++ HW2.R
@@ -0,0 +1,5 @@
+#Homework 2
+# why vaccines do more good than harm
+# The graph wants to explain why vaccines make more benefit than harm for people. 
+# However, I am willing to know why vaccine can be harmful because in my unstanding, veccine can 
+# 
